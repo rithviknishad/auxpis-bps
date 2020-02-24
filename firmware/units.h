7142327aa88e2ea4d7704c0dca3eb16cf6228f9e
Rithvik Nishad <rithvikn2001@gmail.com>
2020-02-24 18:43:14 +0530

    init beta

---
+++ firmware/units.h
@@ -0,0 +1,26 @@
+/*
+    Units definition
+*/
+
+#define unit
+#define milli       * (1e-3)
+
+#define second      unit
+#define ms          milli second
+#define minute      * 60 second
+#define hour        * 60 minute
+
+#define Volt        unit
+#define V           Volt
+#define mV          milli Volt
+
+#define Ampere      unit
+#define A           Ampere
+#define mA          milli Ampere
+
+#define Watt        unit
+#define W           Watt
+#define mW          milli Watt
+
+#define Wh          Watt hour
+#define mWh         milli Wh
