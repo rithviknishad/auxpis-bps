beb9f6becc703c31fc1bc1e97e93b2890bc3cd57
Rithvik Nishad <rithvikn2001@gmail.com>
2020-05-03 18:09:09 +0530

    Finalized

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # auxpis-bps
 AUXPIS - A portable, variable bench power supply, supporting various input sources
 
+## Components
+
+ - IRFZ44N (for Buck)
+ - IRF540N (for Boost)
+ - 33uH Inductor (x2)
+ - 4700uF 35V and 50V Capacitor
+ - ATMEGA328P (Arduino Pro Mini 5V)
+ - Diode (x2)
+ - 12V SMPS
+
 ## Features
  - **CV, CC**
  - Overcurrent / Short Circuit Protection
@@ -13,10 +23,10 @@ AUXPIS - A portable, variable bench power supply, supporting various input sourc
 
 | __Param__ | Min Value | Max Value | Unit |
 | --------- | --------- | --------- | ---- |
-| __Input DC__ | 5 | 50 | **V** |
-| __Input AC__* | 50 | 300 | V |
-| __Output V__ | 1.5 | 26 | **V** |
-| __Max Current__ | - | 3.2 _(m1)_, 6 _(m2)_ | **A** |
+| __Input DC__ | 7 | 30 | **V** |
+| __Input AC__* | 110 | 220 | V |
+| __Output V__ | 2 | 30 | **V** |
+| __Max Current__ | - | 26 | **A** |
 | __Rated Power__ | - | 180 | **W** |
 
 _*AC input should be connected to unit only using the AC-DC adapter. Directly connecting AC inputs to the unit can lead to damage._
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,136 +1,457 @@
-#include <Wire.h>
-#include <LiquidCrystal_SR.h>
+#define VERSION "Version 2020.5.3 [ Build 256 ] --release-build"
 
 #include "units.h"
-#include "GPIO.h"
 
-#define SERIAL_DEBUG // comment this line to disable SERIAL DEBUGGING (for release build)
-#define LCD_DISPLAY // comment this line to disable LCD display (for performance mode, when external system handles display)
+/*  GLOBAL SYSTEM PARAMETERS  */
+#define VOUT_MIN_LIMIT  2   V
+#define VOUT_MAX_LIMIT  30  V
+#define IOUT_MIN_LIMIT  100 mA
+#define IOUT_MAX_LIMIT  26  A
 
-GPIO StatusLED  = GPIO( 13, OUTPUT, HIGH );
 
-GPIO UVLO       = GPIO( 12, OUTPUT, HIGH );
-GPIO BuckGate   = GPIO( 11, OUTPUT );
-GPIO BoostGate  = GPIO( 10, OUTPUT );
-GPIO Boost      = GPIO(  9, OUTPUT, HIGH );
-#ifdef LCD_DISPLAY
-    #define LCD_D        8
-    #define LCD_CLK      7
-    #define LCD_SCK      6
-#endif
-GPIO CV         = GPIO(  5, OUTPUT, HIGH  ); //
-GPIO CC         = GPIO(  4, OUTPUT, HIGH  ); //
-GPIO VPBSense   = GPIO( A0, INPUT );
-GPIO VNBSense   = GPIO( A1, INPUT );
-GPIO ISense     = GPIO( A2, INPUT );
-GPIO VInSense   = GPIO( A3, INPUT );
-GPIO VPOTSense  = GPIO( A4, INPUT );
-GPIO IPOTSense  = GPIO( A5, INPUT );
+/*------------------------  I / O  PINOUT  MAPPING  ------------------------*/
 
-LiquidCrystal_SR screen(LCD_D, LCD_CLK, LCD_SCK);
+#define PIN_STATUS_LED      13
 
-#define VIN_MAX     24 V
-#define VPB_MAX     24 V
-#define VNB_MAX     24 V
-#define VPOT_MIN    3  V
-#define VPOT_MAX    50 V
-#define IPOT_MIN    100 mA
-#define IPOT_MAX    30 A
-#define ISENSE_MAX  75.74 A
-#define UVLOT       7 V
+#define PIN_BOOST_GATE      10
+#define PIN_BUCK_GATE       11
+#define PORT_BUCK           PORTB
+#define BV_BUCK             3
 
-void InitializeTimers() {
-    TCCR2A = (TCCR2A & B11111000) | 1;
-    TCCR1A = (TCCR1A & B11111000) | 1;
+#define PIN_RELAY_N         9
+#define PIN_RELAY_P         9
+
+#define PIN_ACS712_SENSE    A2
+#define PIN_PBOOST_SENSE    A0
+#define PIN_NBOOST_SENSE    A1
+#define PIN_VIN_SENSE       A3   
+
+
+/*------------------------  SERIAL REPORT BIT REGISTER  ------------------------*/
+
+#define SR_REPORT             (1 << 15) //  [ 15 ]  SERIAL REPORT ON / OFF STATE
+#define SR_SRR_BINVAL         (1 << 14) //  [ 14 ]  SRR BIN VAL
+#define SR_UPTIME             (1 << 13) //  [ 13 ]  MILLISECONDS UPTIME
+#define SR_VOUT               (1 << 12) //  [ 12 ]  OP VOLTAGE
+#define SR_IOUT               (1 << 11) //  [ 11 ]  OP CURRENT
+#define SR_VSHUNT             (1 << 10) //  [ 10 ]  SHUNT mV
+#define SR_PWR                (1 << 9)  //  [ 09 ]  OP POWER
+#define SR_ENERGY             (1 << 8)  //  [ 08 ]  OP ENERGY
+#define SR_VIN                (1 << 7)  //  [ 07 ]  INPUT VOLTAGE
+#define SR_TARGET_PARAMETERS  (1 << 6)  //  [ 06 ]  TARGET PARAMETERS (V-MAX, I-MAX)
+#define SR_P_N_VBOOST         (1 << 5)  //  [ 05 ]  P / N BOOST VOLTAGES
+#define SR_                   (1 << 4)  //  [ 04 ] 
+#define SR_BBCMR_BINVAL       (1 << 3)  //  [ 03 ]  OPERATING MODE (BBCMR)
+#define SR_MOSFET_PWM         (1 << 2)  //  [ 02 ]  MOSFET PWM VALUE
+#define SR_THERMALS           (1 << 1)  //  [ 01 ]  THERMALS
+#define SR_EFFICIENCY         (1 << 0)  //  [ 00 ]  EFFICIENCY
+
+unsigned int SRR = SR_REPORT | SR_UPTIME | SR_VOUT | SR_IOUT | SR_VIN | SR_TARGET_PARAMETERS | SR_P_N_VBOOST | SR_BBCMR_BINVAL | SR_MOSFET_PWM | SR_THERMALS | SR_EFFICIENCY;
+
+/*----------------------  OPERATING MODE BIT REGISTER  ----------------------*/
+
+#define BUCK    (1 << 0)
+#define BOOST   (1 << 1)
+#define CV      (1 << 2)
+#define CC      (1 << 3)
+#define UVLO    (1 << 4)
+#define THLO    (1 << 5)
+
+unsigned char BBCMR = 0;
+
+long ACS712_ZeroError = 0;
+
+float Vin = 0 V;
+float UVLOT = 7 V;
+
+float Vout_max = VOUT_MIN_LIMIT V;
+float Iout_max = 500 mA;
+
+float Vnb = 0 V;
+float Vpb = 0 V;
+
+float Vshunt = 0 V;
+float Vout = 0 V;
+float Iout = 0 A;
+
+float Power = 0 W;
+float Energy = 0 Wh;
+
+short PWM_BUCK = 0;
+short PWM_BOOST = 0;
+
+char char_buff_20b[20];  // a handy global buffer space
+int iterator_buff_20b = -1; // corresponding iterator for it :/
+
+
+void InitializeIO()
+{
+    Serial.println("[ .. ] Initializing IO...");
+    
+    // set timer register's prescalers of FET control pins to maximize performance
+    TCCR2A = TCCR2A & B11111000 | B00000001;    // PIN_BOOST_GATE (PIN 10)
+    TCCR1B = TCCR1B & B11111000 | B00000001;    // PIN_BUCK_GATE  (PIN 11)
+    Serial.println("      [ 1 / 5 ] Updated Timer Register Prescalers.");    
+
+    // set pin modes of IO pins (other device's pins initialized and handled by libraries)
+    pinMode(PIN_BOOST_GATE, OUTPUT); pinMode(PIN_BUCK_GATE, OUTPUT); pinMode(PIN_STATUS_LED, OUTPUT); // DDRB |= ((1 << 4) | (1 << 5) | (1 << 7));
+    digitalWrite(PIN_BOOST_GATE, LOW); digitalWrite(PIN_BUCK_GATE, LOW); digitalWrite(PIN_STATUS_LED, HIGH); // PORTB = (PORTB & ~((1 << 4) | (1 << 5))) | (1 << 7);  
+    Serial.println("      [ 2 / 5 ] Initialized { BOOST , BUCK } N-MOSFET_GATE_DRIVE_PIN registers.");
+
+    pinMode(PIN_RELAY_P, OUTPUT); pinMode(PIN_RELAY_N, OUTPUT); // DDRA |= ((1 << 0) | (1 << 1));  
+    digitalWrite(PIN_RELAY_P, HIGH); digitalWrite(PIN_RELAY_N, HIGH); // PORTA |= ((1 << 0) | (1 << 1));  
+    Serial.println("      [ 3 / 5 ] Initialized PIN_RELAY { P , N } registers.");
+    
+    pinMode(PIN_VIN_SENSE, INPUT); pinMode(PIN_ACS712_SENSE, INPUT); pinMode(PIN_NBOOST_SENSE, INPUT); pinMode(PIN_PBOOST_SENSE, INPUT);  // DDRF &= ~((1 << 4) | (1 << 5) | (1 << 6) | (1 << 7));   
+    Serial.println("      [ 4 / 5 ] Initialized PIN_SENSE registers.");
+    
+    // pinMode(PIN_LM35_REG_SENSE, INPUT); pinMode(PIN_LM35_FET0_SENSE, INPUT); pinMode(PIN_LM35_FET1_SENSE, INPUT); // DDRK &= ~((1 << 0) | (1 << 1) | (1 << 2)); 
+    // Serial.println("      [ 5 / 5 ] Initialized PIN_LM35_THERMAL_SENSE registers.");
+    
+    Serial.println("[ OK ] IO registers initialized and updated.");
 }
 
-void setup() {
-    InitializeTimers();
-    
-#ifdef LCD_DISPLAY
-    screen.begin(16, 2);
-    screen.home();
-    screen.print("     AUXPIS");
-    screen.setCursor(0, 1);
-    screen.print("PWR SPLY   v 1.2");
-#endif
-#ifdef SERIAL_DEBUG
+void InitializeACS712()
+{
+    Serial.println("[ .. ] Initializing ACS712 Bidirectional Current Sensor (ELC30A)");
+
+    for (int i = 0; i < 100; ++i)
+        ACS712_ZeroError += analogRead(PIN_ACS712_SENSE);
+    ACS712_ZeroError /= 100;
+
+    Serial.print("       Sensor::ACS712::Zero_Error=");
+    Serial.println(ACS712_ZeroError);
+
+    Serial.println("[ OK ] ACS712 Sensor Initialized.");
+}
+
+void InitializeSystem()
+{
     Serial.begin(115200);
-#endif
+
+    Serial.println("\n AUXPIS POWER SUPPLY");
+    Serial.println(VERSION);
+    Serial.println("Licensed under MIT License. See LICENSE.md at https://github.com/rithviknishad/auxpis-bps/\n");
+
+    InitializeIO();
+    InitializeACS712();
+    delay(1000);
+    /// add restriction code for low inupt voltage...
 }
 
-float VIn, MaxOPV, OPV, MaxOPI, OPI, OPPower, OPEnergy, VPB, VNB;
-int GATE_PWM = 0;
 
-#ifdef SERIAL_DEBUG
-    #define LOG(x)   Serial.print(x); Serial.print(',');
-    #define LOGEnd(x)   Serial.print(x); Serial.print('\n')
-    #define LOGCSV(a, b, c, d, e, f, g, h, i, j, k, l) LOG(a) LOG(b) LOG(c) LOG(d) LOG(e) LOG(f) LOG(g) LOG(h) LOG(i) LOG(j) LOG(k) LOGEnd(l)
-#endif
+void setup()
+{
+    InitializeSystem();
 
-#ifdef LCD_DISPLAY
-void UpdateInfotainment(LiquidCrystal_SR &lcd) {    
-    lcd.setCursor(0, 0);
-    int remch = lcd.print("OP V = ");
-    remch += lcd.print(OPV);
-    remch += lcd.print(" V");
-    while(remch < 17)
-        remch += lcd.print(" ");
+    Serial.println("AUXPIS Power Supply is ready for use.");
 
-    lcd.setCursor(0, 1);
-    remch -= lcd.print("OP I = ");
-    remch -= lcd.print(OPI);
-    remch -= lcd.print(" A");
-    while (remch > 0)
-        remch -= lcd.print(" ");
+    PORTB &= ~(1 << 7);
 }
-#endif
 
-void UpdateExternal(unsigned long deltaTime_millis) {
-    MaxOPV = (analogRead(VPOTSense.pin) * (VPOT_MAX - VPOT_MIN) / 1023.00) + VPOT_MIN;
-    MaxOPI = (analogRead(IPOTSense.pin) * (IPOT_MAX - IPOT_MIN) / 1023.00) + IPOT_MIN;
+
+float set_Vout_max(float value)
+{
+    if (value > VOUT_MAX_LIMIT)
+        return Vout_max = VOUT_MAX_LIMIT;
+    else if (value < VOUT_MIN_LIMIT)
+        return Vout_max = VOUT_MIN_LIMIT;
+    else
+        return Vout_max = value;
+}
+
+float getValueFromString(char * str)
+{
+    float value = 0;
+    unsigned char decimalOffset = strlen(str);
+
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        if (str[i] > 47 && str[i] < 58)
+        {
+            value *= 10;
+            value += str[i] - 48;
+        }
+        else if (str[i] == '.')
+            decimalOffset = i + 1;
+        else
+            return -1;
+    }
+    return (value * pow(0.1, strlen(str) - decimalOffset));
+}
+
+float set_Iout_max(float value)
+{
+    if (value > IOUT_MAX_LIMIT)
+        return Iout_max = IOUT_MAX_LIMIT;
+    else if (value < IOUT_MIN_LIMIT)
+        return Iout_max = IOUT_MIN_LIMIT;
+    else
+        return Iout_max = value;
+}
+
+char* readStringFromStream(Stream &stream, char* buffer, int max_size = 20)
+{
+    int i = -1;
+    while(i < max_size)
+    {
+        if (stream.available() > 0)
+        {
+            Serial.print(buffer[++i] = stream.read());
+            if (buffer[i] == ';' || buffer[i] == '\n' || buffer[i] == '\r')
+            {
+                buffer[i] = '\0';
+                return buffer;
+            }
+        }
+    }
+    return buffer = "";
+}
+
+void (*SYS_RESET) (void) = 0;
+
+void UpdateSerialReport()
+{
+    if (Serial.available() > 0)
+    {
+        static char readByte = 0;
+        switch (readByte = Serial.read())
+        {
+        case 'V':
+        case 'v':
+            Serial.print("\nSet OP Voltage [V] : ");            
+            Serial.print("\nVout_max set to : ");
+            Serial.print(set_Vout_max(getValueFromString(readStringFromStream(Serial, char_buff_20b))));
+            Serial.println(" V");
+            break;
+        
+
+        case 'I':
+        case 'i':
+            Serial.print("\nSet Max Current [mA] : ");
+            Serial.print("\nMax Current set to : ");
+            Serial.print(set_Iout_max(getValueFromString(readStringFromStream(Serial, char_buff_20b))));
+            Serial.println(" A");
+            break;
+
+        case 'R':
+        case 'r':
+            SYS_RESET();
+            break;
+
+        case 'S':
+            SRR = 0;
+            Serial.print("\nEnter SRR Register Values: ");
+            
+            for (int i = 15; i > -1; --i)
+            {
+                if (Serial.available() > 0)
+                    SRR |= ((Serial.read() - 48 ? 1 : 0) << i);
+            }
+
+            Serial.print("\nSRR=");
+            Serial.println(SRR, BIN);
+
+        default:
+            break;
+        }
+    }
+
+
+    if (SRR & SR_REPORT)
+    {
+        if (SRR & SR_SRR_BINVAL)
+        {
+            Serial.print(SRR, BIN);
+            Serial.print(" ");
+        }
+
+        if (SRR & SR_UPTIME)
+        {
+            Serial.print(millis());
+            Serial.print("ms  ");
+        }
+
+        if (SRR & SR_VOUT)
+        {
+            Serial.print("V=");
+            Serial.print(Vout);
+            Serial.print(" V  ");
+        }
+
+        if (SRR & SR_IOUT)
+        {
+            Serial.print("I=");
+            Serial.print((float)((Iout < 0.0) ? 0 : Iout));
+            Serial.print(" A  ");
+        }
+
+        if (SRR & SR_VSHUNT)
+        {
+            Serial.print("Vshunt=");
+            Serial.print(Vshunt);
+            Serial.print(" mV  ");
+        }
+
+        if (SRR & SR_PWR)
+        {
+            Serial.print("P=");
+            Serial.print(Power);
+            Serial.print(" W  ");
+        }
+
+        if (SRR & SR_ENERGY)
+        {
+            Serial.print("E=");
+            Serial.print(Energy);
+            Serial.print(" Wh  ");
+        }
+
+        if (SRR & SR_VIN)
+        {
+            Serial.print("Vi=");
+            Serial.print(Vin);
+            Serial.print(" V  ");
+        }
+
+        if (SRR & SR_TARGET_PARAMETERS)
+        {
+            Serial.print("Vm=");
+            Serial.print(Vout_max);
+            Serial.print("V  Im=");
+            Serial.print(Iout_max);
+            Serial.print("A  ");
+        }
+
+        if (SRR & SR_P_N_VBOOST)
+        {
+            Serial.print("PB=");
+            Serial.print(Vpb);
+            Serial.print("V NB=");
+            Serial.print(Vnb);
+            Serial.print("V  ");
+        }
+
+        if (SRR & SR_BBCMR_BINVAL)
+        {
+            Serial.print("BBCMR=");
+            Serial.print(BBCMR, BIN);
+            Serial.print(" ");
+        }
+        
+        if (SRR & SR_MOSFET_PWM)
+        {
+            Serial.print("~BUK=");
+            Serial.print(PWM_BUCK, DEC);
+            Serial.print(" ~BST=");
+            Serial.print(PWM_BOOST, DEC);
+            Serial.print(" ");
+        }
+
+        if (SRR & SR_THERMALS)
+        {
+
+        }
+
+        if (SRR & SR_EFFICIENCY)
+        {
+
+        }
+
+        Serial.println(";");
+    }
+}
+
+// stores the last update time for external activities
+unsigned long external_last_millis = 0;
+//unsigned long internal_last_millis = 0;
+
+#define vin_mfact (0.0284330659387831)
+#define vnb_mfact (0.0284330659387831)
+#define vpb_mfact (0.5682193890319093)
+
+#define analogVoltage(x)            ((x) * 5.00f / 1023.00f)
+#define ACS712_SENSITIVITY          (66e-3)
+
+float getCurrentFromACS712()
+{
+    Vshunt = (float)(ACS712_ZeroError - analogRead(PIN_ACS712_SENSE)) * 5.00 / 1023.00;
+    return Vshunt / ACS712_SENSITIVITY;
+}
+
+#define IsCC (Iout >= Iout_max)
+#define IsCV (Vout >= Vout_max * 0.98)
+
+void UpdateExternals()
+{
+    static unsigned short delta_millis_external = millis() - external_last_millis;
     
-    UVLO = VIn < UVLOT;   
-    Boost = MaxOPV > VIn;
+    Power = Vout * Iout;
+    Energy += (Power < 0 ? 0 : Vout * Iout * delta_millis_external / 3600);
+    external_last_millis += delta_millis_external;
 
-    OPPower = OPV * OPI;
-    OPEnergy += OPPower * deltaTime_millis ms;
+    UpdateSerialReport();
+    
+    if (Vout_max < Vin)
+    {
+        BBCMR |= BUCK;
+        BBCMR &= ~BOOST;
+    }
+    else
+    {
+        BBCMR |= BOOST;
+        BBCMR &= ~BUCK;
+    }
 
-#ifdef LCD_DISPLAY
-    UpdateInfotainment(screen);
-#endif
-#ifdef SERIAL_DEBUG
-    LOGCSV(VIn, millis(), MaxOPV, OPV, MaxOPI, OPI, OPPower, OPEnergy, VPB, VNB, GATE_PWM, " ");
-#endif
+    BBCMR = ((Vin < UVLOT) ? ((BBCMR & ~(BOOST | BUCK)) | UVLO) : (BBCMR & ~UVLO)); // under voltage lockout protection
+
+    digitalWrite(PIN_RELAY_N, !(BBCMR & BOOST));
+    //PORTA = ((BBCMR & BUCK ) ? (PORTA & ~(1 << 0)) : (PORTA & ~(1 << 1)));  // SET RELAY SIGNAL ACCORDING TO BUCK / BOOST
+    //PORTA = ((BBCMR & BOOST) ? (PORTA | (1 << 0)) : (PORTA | (1 << 1)));
 }
 
-void UpdateInternal() {
-    VIn = analogRead( VInSense.pin ) * VIN_MAX / 1023.00;
-    VPB = analogRead( VPBSense.pin ) * VPB_MAX / 1023.00;
-    VNB = analogRead( VNBSense.pin ) * VNB_MAX / 1023.00;
+
+#define BUCK_HIGHER     (PORT_BUCK |=  (1 << BV_BUCK))  //analogWrite(PIN_BUCK_GATE , (( PWM_BUCK >=  220) ? (PWM_BUCK=220 ) : (++PWM_BUCK ) ))
+#define BUCK_LOWER      (PORT_BUCK &= ~(1 << BV_BUCK)) //analogWrite(PIN_BUCK_GATE , (( PWM_BUCK >    0 ) ? ( --PWM_BUCK  ) : (PWM_BUCK=0 ) ))
+#define BOOST_HIGHER    analogWrite(PIN_BOOST_GATE, (( PWM_BOOST >= 220) ? (PWM_BOOST=220) : (++PWM_BOOST) ))
+#define BOOST_LOWER     analogWrite(PIN_BOOST_GATE, ((PWM_BOOST >    0 ) ? ( --PWM_BOOST ) : (PWM_BOOST=0) ))
+
+void UpdateInternals()
+{
+    // get delta time from last update
+    //static unsigned short delta_millis_internal = millis() - internal_last_millis;
+
+    Vin = analogRead(PIN_VIN_SENSE) * vin_mfact;
+    Vpb = analogRead(PIN_PBOOST_SENSE) * vpb_mfact;
+    Vnb = analogRead(PIN_NBOOST_SENSE) * vnb_mfact;
     
-    OPV = (Boost ? VPB : (VIn - VNB));
-    OPI = (analogRead(ISense.pin) * ISENSE_MAX * 2 / 1023.00) - ISENSE_MAX;
+    Vout = (BBCMR & BOOST) ? Vpb : (Vin - Vnb);
+    
+    Iout = getCurrentFromACS712();
 
-    CV = !(OPV < MaxOPV);
-    CC = !(OPI < MaxOPI);
+    BBCMR = ((Vout >= Vout_max) ? (BBCMR | CV) : (BBCMR & ~CV));  //CV register update
+    BBCMR = ((Iout >= Iout_max) ? (BBCMR | CC) : (BBCMR & ~CC));  //CC register update
 
-    if (CV | CC)  --GATE_PWM;  else  ++GATE_PWM;
+    // update buck channel
+    if ((BBCMR & BUCK))
+        ((IsCC || IsCV) ? BUCK_LOWER : BUCK_HIGHER);
+    else if (PWM_BUCK)
+        analogWrite(PIN_BUCK_GATE, PWM_BUCK = 0);  // single channel locking
 
-    analogWrite( BoostGate.pin, (( Boost && !UVLO) ? (GATE_PWM = min(220, GATE_PWM)) : 0) );
-    analogWrite( BuckGate.pin,  ((!Boost && !UVLO) ? (GATE_PWM = min(255, GATE_PWM)) : 0) );
-} 
+    // update boost channel
+    if ((BBCMR & BOOST))
+        ((IsCC || IsCV) ? BOOST_LOWER : BOOST_HIGHER);
+    else if (PWM_BOOST)
+        analogWrite(PIN_BOOST_GATE, PWM_BOOST = 0);  // single channel locking
+}
 
-void loop() {
-#if defined(SERIAL_DEBUG) || defined(LCD_DISPLAY)
-    static unsigned long external_last_millis = 0, delta_external_miilis = 0;
+void loop()
+{
+    UpdateInternals();
 
-    UpdateExternal( millis() - external_last_millis );
-    external_last_millis = millis();
-
-    while( millis() - external_last_millis < 200 )
-        UpdateInternal();
-#else
-    UpdateInternal();
-#endif
+    if (millis() - external_last_millis > 200)
+    {
+        UpdateExternals();
+        external_last_millis = millis();
+    }
 }
